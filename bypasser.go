fe3cb02b3ee025c738e4f9927db5b5a32a8c9627
VinsmokeStone <76612747+VinsmokeStone@users.noreply.github.com>
2023-09-24 18:26:52 +0900

    Add files via upload

--- bypasser.go
+++ bypasser.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/shirou/gopsutil/process"
@@ -10,32 +11,36 @@ import (
 func main() {
 	fmt.Println("Made by DevaAndSage.")
 	fmt.Println("5초 뒤에 우회가 시작됩니다.")
-
+	// 일시 중지할 프로세스 이름 리스트
 	// 5초 동안 멈춥니다.
 	duration := 5 * time.Second
 	time.Sleep(duration)
+	processNames := []string{"iAgent.exe", "iAgent32.exe", "iWatcher.exe", "iService.exe"}
 
-	fmt.Println("우회 시도중...")
+	// 현재 실행 중인 모든 프로세스 목록 가져오기
+	processList, err := process.Processes()
+	if err != nil {
+		fmt.Printf("프로세스 목록을 가져오는 동안 오류 발생: %v\n", err)
+		return
+	}
 
-	// 일시 중단할 프로세스들의 PID 리스트를 지정합니다.
-	targetPIDs := []int{12124, 12156, 5096, 5008} // 변경해야 할 PID 리스트로 설정하세요.
+	// 입력한 각 프로세스 이름에 대해 프로세스를 찾아서 중지
+	for _, processName := range processNames {
+		for _, p := range processList {
+			name, err := p.Name()
+			if err != nil {
+				continue
+			}
 
-	// 각 프로세스에 대해 작업을 수행합니다.
-	for _, targetPID := range targetPIDs {
-		targetProcess, err := process.NewProcess(int32(targetPID))
-		if err != nil {
-			fmt.Printf("프로세스 %d를 가져오는 데 오류 발생: %v\n", targetPID, err)
-			continue // 다음 프로세스를 시도합니다.
+			if strings.Contains(strings.ToLower(name), strings.ToLower(processName)) {
+				err := p.Suspend()
+				if err == nil {
+					fmt.Printf("프로세스 %s (PID %d) 우회 성공.\n", name, p.Pid)
+				} else {
+					fmt.Printf("프로세스 %s (PID %d) 우회 실패 관리자 권한으로 실행해주세요: %v\n", name, p.Pid, err)
+				}
+			}
 		}
-
-		// 프로세스를 일시 중단합니다.
-		err = targetProcess.Suspend()
-		if err != nil {
-			fmt.Printf("프로세스 %d를 일시 중단하는 데 오류 발생: %v\n", targetPID, err)
-		} else {
-			fmt.Printf("프로세스 %d가 일시 중단되었습니다.\n", targetPID)
-		}
-
 	}
 	fmt.Println("프로그램을 종료하려면 엔터 키를 누르세요.")
 	fmt.Scanln() // 엔터 키를 누를 때까지 대기
